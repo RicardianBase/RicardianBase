e48c1b9a5b044f959980c424a8ee282c2ff6381c
RicardianBase <ricardian1@proton.me>
2026-04-01 16:57:44 +0000

    Changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,5 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import VortexHero from "@/components/vortex/VortexHero";
-import ImageMarquee from "@/components/vortex/ImageMarquee";
-import TestimonialQuote from "@/components/vortex/TestimonialQuote";
-import PricingSection from "@/components/vortex/PricingSection";
-import TestimonialCarousel from "@/components/vortex/TestimonialCarousel";
-import ProjectsSection from "@/components/vortex/ProjectsSection";
-import PartnerCTA from "@/components/vortex/PartnerCTA";
-import VortexFooter from "@/components/vortex/VortexFooter";
-import FixedBottomNav from "@/components/vortex/FixedBottomNav";
 import TrustStatement from "@/components/ricardian/TrustStatement";
 import ServicesGrid from "@/components/ricardian/ServicesGrid";
 import OrbitalTestimonials from "@/components/ricardian/OrbitalTestimonials";
@@ -23,16 +14,6 @@ const Index = () => {
     <div className="relative" style={{ fontFamily: "'PP Neue Montreal', -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif" }}>
       <Navbar />
       <HeroSection />
-      <VortexHero />
-      <ImageMarquee />
-      <TestimonialQuote />
-      <PricingSection />
-      <TestimonialCarousel />
-      <ProjectsSection />
-      <PartnerCTA />
-      <VortexFooter />
-
-      {/* RicardianBase Sections */}
       <TrustStatement />
       <ServicesGrid />
       <OrbitalTestimonials />
@@ -41,8 +22,6 @@ const Index = () => {
       <StatsShowcase />
       <EducationalResources />
       <RicardianFooter />
-
-      <FixedBottomNav />
     </div>
   );
 };
